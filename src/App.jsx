5582a53755aca9262d9bd4e11cd4aa5d55ddc536
elham saremi <elhamsaremi88@gmail.com>
2023-11-06 01:51:34 +0330

    feat: edit , delete

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,63 @@ import TaskCard from "./components/TaskCard/TaskCard";
 const existingTasks = JSON.parse(localStorage.getItem("tasks"));
 function App() {
   const [tasks, setTasks] = useState(existingTasks ?? []);
-  const [count, setCount] = useState(0);
+
+  const [category, setCategory] = useState("done");
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
   const handleAddTask = (taskTitle) => {
     const newTask = {
       title: taskTitle,
+      done: false,
       id: Date.now(),
     };
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
+  const handleDelete = (id) => {
+    setTasks((prevTasks) => prevTasks.filter((t) => t.id !== id));
+  };
+
+  const handleEdit = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((t) => (t.id === id ? { ...t, done: !t.done } : t))
+    );
+  };
+
+  const handleSelect = (value) => {
+    setCategory(value);
+  };
+
+  /* const filteredTasks = () => {
+    if (category !== "all") {
+      return tasks.filter((t) => (category === "done" ? t.done : !t.done));
+    } else return tasks;
+  };*/
+
+  const filteredTasks =
+    category !== "all"
+      ? tasks.filter((t) => (category === "done" ? t.done : !t.done))
+      : tasks;
+
   return (
     <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>add</button>
-      </div>
       <div className=" w-96 mx-auto  bg-blue-400 h-screen">
         <div className="flex gap-2 mt-10 p-4 ">
           <Input onAddTask={handleAddTask} />
-          <Select />
+          <Select onSelect={handleSelect} value={category} />
         </div>
 
         <div className=" flex flex-col gap-5 w-full p-3">
-          {tasks.map((task) => (
-            <TaskCard key={task.id} task={task} />
+          {filteredTasks.map((task) => (
+            <TaskCard
+              key={task.id}
+              task={task}
+              onDelete={handleDelete}
+              onEdit={handleEdit}
+            />
           ))}
         </div>
       </div>
--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,12 +1,26 @@
 import { PropTypes } from "prop-types";
-function TaskCard({ task }) {
+function TaskCard({ task, onDelete, onEdit }) {
   return (
     <div className="flex w-full gap-2  ">
-      <h2 className="bg-white p-1 flex-1 flex items-center">{task.title}</h2>
-      <button className="bg-green-700 text-white p-2">
+      <h2
+        className={`bg-white p-1 flex-1 flex items-center ${
+          task.done ? "line-through" : ""
+        }`}
+      >
+        {task.title}
+      </h2>
+      <button
+        onClick={() => onEdit(task.id)}
+        className={`${
+          task.done ? "bg-green-500" : "bg-green-900"
+        } text-white p-2`}
+      >
         <i className="bi bi-check-lg"></i>
       </button>
-      <button className="bg-red-700 text-white p-2">
+      <button
+        onClick={() => onDelete(task.id)}
+        className="bg-red-700 text-white p-2"
+      >
         <i className="bi bi-trash3"></i>
       </button>
     </div>
